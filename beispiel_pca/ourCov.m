425cbfdeb8f0f6b0e4f5945499e558a952002c09
eh5an <one9_8one@yahoo.com>
2014-05-27 14:31:44 +0200

    ourcov added

---
+++ beispiel_pca/ourCov.m
@@ -0,0 +1,16 @@
+function [C]=ourCov(D)
+D=D';
+[n,d]=size(D);
+mm=mean(D);
+for i=1:d
+    for j=1:d
+        temp=0;
+        for k=1:n
+        temp=temp+((D(k,i)-mm(i))*(D(k,j)-mm(j)));
+        if k==n;
+            temp=temp/(n-1);
+            C(i,j)=temp;
+        end  
+        end
+    end
+end
