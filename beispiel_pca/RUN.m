da2dce68e6183f91470e8a889939163796588d57
eh5an <one9_8one@yahoo.com>
2014-05-27 16:27:49 +0200

    change

--- beispiel_pca/RUN.m
+++ beispiel_pca/RUN.m
@@ -1,11 +1,11 @@
 function RUN
 close all
 clear
-a;
+Aufgabe1;
 end
 
 % Aufgabe 1 a&b %
-function a
+function Aufgabe1
 load('daten.mat');
 D1=ourCov(data1);
 D2=ourCov(data2);
@@ -13,16 +13,16 @@ D3=ourCov(data3);
 D4=ourCov(data4);
 
 
-plot(data1','.')
+plot(data1(1,:), data1(2,:), '.')
 axis equal
 figure
-plot(data2','.')
+plot(data2(1,:), data2(2,:), '.')
 axis equal
 figure
-plot(data3','.')
+plot(data3(1,:), data3(2,:), '.')
 axis equal
 figure
-plot(data4','.')
+plot(data4(1,:), data4(2,:), '.')
 axis equal
 end
 
